006fad1ee8bc721772eb93d8e7c6e410ac89f90f
Paky <paky.ce@hotmail.com>
2023-04-03 17:03:59 +0200

    esercizio g1 week2

---
+++ Settimana 2/Giorno 1/assets/Js/script.js
@@ -0,0 +1,9 @@
+alert('alert1')
+
+alert('alert2')
+
+document.getElementById('title').innerHTML = 'Iniziamo la nostra pratica';
+
+document.getElementById('output').innerHTML = 'Output scritto da javascript';
+
+document.getElementById('panterat').innerHTML = 'Panterat scritto da javascript';
---
+++ Settimana 2/Giorno 1/assets/Js/script2.js
@@ -0,0 +1,8 @@
+var criceto = 'criceto';
+console.log(criceto);
+
+var giraffa = 'giraffa';
+console.log(giraffa);
+
+var leone = 'leone';
+console.log(leone);
